0cb8356c2facbf5cae969a08b0a816bb5ba56dff
Chiraitori <77224364+tathaha@users.noreply.github.com>
2024-03-16 23:12:31 +0700

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,175 @@
+<template>
+  <div>
+    <h1></h1>
+    <div class="box">
+      <div class="box1">
+        <table class="table1" id="mytable">
+          <th>STT</th>
+          <th>Nguyên Vật Liệu</th>
+          <th>Số lượng</th>
+          <th>Giá cả (VND)</th>
+          <tr>
+            <td>1</td>
+            <td>Gà</td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Cám</td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Trứng</td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Công Nhân</td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+          </tr>
+        </table>
+      </div>
+      <br />
+      <br />
+      <div class="box2">
+        <table class="table2" id="myTable">
+          <tr>
+            <th>STT</th>
+            <th>Nguyên Vật Liệu</th>
+            <th>Số lượng</th>
+            <th>Giá cả (VND)</th>
+          </tr>
+          <tr v-for="(row, index) in rows" :key="index" :class="`row-${index}`">
+            <td>{{ index + 1 }}</td>
+            <td><input type="text" placeholder="Nhập dữ liệu" /></td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+            <td><input type="number" placeholder="Nhập dữ liệu" /></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <button class="button1" @click="themHang()">Thêm Hàng</button>
+    <button class="button2">Nộp Báo Cáo</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rows: []
+    }
+  },
+  methods: {
+    themHang() {
+      this.rows.push({})
+    }
+  }
+}
+</script>
+
+<style scoped>
+body {
+  width: 100%;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+.box1 {
+  padding-top: 80px;
+  display: flex;
+  margin: 0;
+  left: 0;
+  align-items: center;
+  justify-content: center;
+  gap: 100px;
+}
+
+.box2 {
+  display: flex;
+  margin: 0;
+  left: 0;
+  align-items: center;
+  justify-content: center;
+}
+
+table,
+th,
+td {
+  border: 1px solid #333;
+  border-collapse: collapse;
+  text-align: center;
+}
+input {
+  border: rgb(236, 244, 175);
+  box-sizing: border-box;
+  width: 100%;
+  text-align: center;
+  background-color: rgb(236, 244, 175);
+}
+.button1 {
+  height: 30px;
+  width: 100px;
+  background-color: bisque;
+  border-radius: 20px;
+  justify-content: center;
+  margin-left: 46%;
+  margin-top: 20px;
+}
+.button1:hover {
+  background-color: antiquewhite;
+  transition: background-color 0.2s ease;
+}
+.button1:active {
+  background-color: gray;
+  transition: background-color 0.2s ease;
+}
+.button2 {
+  height: 30px;
+  width: 100px;
+  background-color: bisque;
+  justify-content: center
+  border-radius: 20px;
+  margin-left: 46%;
+  margin-top: 10px;
+}
+.button2:hover {
+  background-color: antiquewhite;
+  transition: background-color 0.2s ease;
+}
+.button2:active {
+  background-color: gray;
+  transition: background-color 0.2s ease;
+}
+.table1 th {
+  background-color: aquamarine;
+}
+.table1 td {
+  background-color: rgb(236, 244, 175);
+}
+.table2 th {
+  background-color: aquamarine;
+}
+.table2 td {
+  background-color: rgb(236, 244, 175);
+}
+/* Add new row styles */
+.table2 .row-0 td {
+  background-color: rgb(236, 244, 175);
+}
+.table2 .row-1 td {
+  background-color: rgb(236, 244, 175);
+}
+.table2 .row-2 td {
+  background-color: rgb(236, 244, 175);
+}
+.table2 .row-3 td {
+  background-color: rgb(236, 244, 175);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h1>Welcome to Chicken's Company</h1>
+    <div class="wrapper"></div>
+    <form>
+      <br />
+      <br />
+      <h2>
+        <div class="form-field">
+          <input
+            type="text"
+            class="form-input"
+            name="user"
+            id="user"
+            size="29"
+            placeholder=" "
+            required
+          />
+          <label for="name" class="form-label">Tài Khoản đăng nhập</label>
+        </div>
+      </h2>
+      <br />
+      <br />
+      <div class="form-field">
+        <input
+          type="password"
+          class="form-input"
+          name="password"
+          id="password"
+          size="29"
+          placeholder=" "
+          required
+        />
+        <label for="name" class="form-label">Mật khẩu đăng nhập</label>
+        <br />
+      </div>
+      <br />
+      <div>
+        <button class="button" type="button" @click="accountpassword()">Đăng nhập</button>
+        <p class="textwarning" id="Result"></p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    accountpassword() {
+      const list = [
+        { user: 'morriskayer', password: '210505' },
+        { user: 'chococomilk', password: '140305' },
+        { user: 'thienphu', password: '210505210505' }
+      ]
+      var user = document.getElementById('user').value
+      var password = document.getElementById('password').value
+      if (list.some((item) => item.user === user && item.password === password)) {
+        window.location.href = 'about'
+        window.close()
+      } else if (user === '' && password === '') {
+        document.getElementById('Result').innerHTML = 'Vui lòng nhập Tài khoản và Mật Khẩu!'
+      } else {
+        document.getElementById('Result').innerHTML =
+          'Thông tin đăng nhập không đúng. Vui lòng kiểm tra lại'
+      }
+    }
+  }
+}
+</script>
