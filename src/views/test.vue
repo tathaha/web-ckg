c6aa62c07331eeab3e981990594782f43e39c66c
Chiraitori <77224364+tathaha@users.noreply.github.com>
2024-04-01 22:24:53 +0700

    add excel but in csv

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="wrapper-dgxl">
     <div ref="dgxl" class="grid"></div>
-    <input type="button" value="Add new row" @click="dgxlObj.insertEmptyRows()" />
-    <input type="button" value="Download data as CSV" @click="dgxlObj.downloadDataAsCSV()" /><br />
+    <input type="button" class="buttons" value="Add new row" @click="dgxlObj.insertEmptyRows()" />
+    <input
+      type="button"
+      class="buttons"
+      value="Download data as CSV"
+      @click="dgxlObj.downloadDataAsCSV()"
+    /><br />
   </div>
 </template>
 
@@ -12,16 +17,50 @@ import DataGridXL from '@datagridxl/datagridxl2'
 export default {
   name: 'DataGrid',
   data() {
-    return {}
+    return {
+      time: null
+    }
   },
   computed: {
     dgxlOptions() {
-      return {}
+      return {
+        columns: [
+          {
+            title: 'Nguyên Vật Liệu'
+          },
+          {
+            title: 'Số lượng'
+          },
+          {
+            title: 'Giá cả (VND)'
+          },
+          {
+            title: 'Ghi chú'
+          },
+          {
+            title: 'set time'
+          }
+        ]
+      }
     }
   },
   mounted: function () {
     const dgxlObj = new DataGridXL(this.$refs.dgxl, this.dgxlOptions)
-    Object.assign(this, { dgxlObj }) // tucks all methods under dgxlObj object in component instance
+    Object.assign(this, { dgxlObj })
+
+    this.getTimeFromServer()
+  },
+  methods: {
+    getTimeFromServer() {
+      fetch('asia.pool.ntp.org') // Replace with the actual time server URL
+        .then((response) => response.json())
+        .then((data) => {
+          this.time = data.time // Assuming the time server returns the time in the "time" field
+        })
+        .catch((error) => {
+          console.error('Error fetching time from server:', error)
+        })
+    }
   }
 }
 </script>
@@ -29,5 +68,21 @@ export default {
 <style>
 .grid {
   height: 400px;
+  width: 95%;
+  margin: 0 auto;
+  border: solid black 1px;
+  border-radius: 10px;
+  margin-top: 100px;
+}
+.buttons {
+  margin-top: 10px;
+  margin-left: auto;
+  margin-bottom: 2rem;
+  margin-right: auto;
+  display: block;
+  height: 60px;
+  width: 200px;
+  border-radius: 20px;
+  background-color: bisque;
 }
 </style>
